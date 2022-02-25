90b7fd2048c8aa42589e99a47549bd631e68436b
Geraldine Castillo <geriraver@gmail.com>
2022-02-25 18:41:42 -0300

    database with env

--- dataBase/dataBase.go
+++ dataBase/dataBase.go
@@ -1,6 +1,7 @@
 package dataBase
 
 import (
+	"collector/config"
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -8,7 +9,7 @@ import (
 
 // Returns an open connection
 func Conectar() *sql.DB {
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/colector")
+	db, err := sql.Open("mysql", config.Get("SQL_USER")+":"+config.Get("SQL_PASSWORD")+"@tcp(127.0.0.1:3306)/collector")
 	if err != nil {
 		panic(err.Error())
 	}
