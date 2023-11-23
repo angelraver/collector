f153924130d8db0022642082babcd12dac18fad2
angelraver <angeldavidflorentin@gmail.com>
2023-11-23 18:22:49 -0300

    Making this just api

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 	var router Router
 	fmt.Println("Starting server on 8001...")
-	http.ListenAndServe(":8001", router)
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	http.ListenAndServe(":8001", handlers.CORS(originsOk, headersOk, methodsOk)(router))
 }
